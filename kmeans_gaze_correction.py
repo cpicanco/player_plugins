a0ee90233e95a8bea38e0ae6a6bcb804f07a9dd8
cpicanco <cpicanco@ufpa.br>
2015-11-19 15:12:39 -0300

    gaze coorection early design

--- kmeans_gaze_correction.py
+++ kmeans_gaze_correction.py
@@ -9,6 +9,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+# modified from manual_gaze_correction
+
 import cv2
 from copy import deepcopy
 from plugin import Plugin
@@ -18,18 +20,20 @@ from pyglui import ui
 import logging
 logger = logging.getLogger(__name__)
 
+# todo: test https://en.wikipedia.org/wiki/DBSCAN, or alternative clustering algorithms.
+
 class KMeans_Gaze_Correction(Plugin):
     """
         Correct gaze by:
         -1) assuming equaly distributed k stimuli at a detected screen (in the world frame);
-        0) screen center and gaze points is given from homographic trasformation 
+        0) screen center and gaze points are given from homographic trasformation 
         1) grouping gaze in blocks with arbitrary size;
         2) infering bias from the difference between the mean k-means clustering and the screen center
-        3) corrections is the screen center subtracted from bias
+        3) correction is the screen center subtracted from bias
     """
 
     def __init__(self,g_pool, k=2):
-        super(Manual_Gaze_Correction, self).__init__(g_pool)
+        super(KMeans_Gaze_Correction, self).__init__(g_pool)
         #let the plugin work before most other plugins.
         self.order = .3
         self.menu = None
@@ -37,6 +41,12 @@ class KMeans_Gaze_Correction(Plugin):
         self.untouched_gaze_positions_by_frame = deepcopy(self.g_pool.gaze_positions_by_frame)
         self._k = k
         self._set_offset()
+
+    def _get_bias_by_frame(self, frame, gaze):
+        pass
+
+    def _get_screen_center(self):
+        pass
 
     def _set_k(self, kvalue):
         self._k = kvalue
@@ -48,8 +58,18 @@ class KMeans_Gaze_Correction(Plugin):
         # ymax = 768
         # xmin = 0
         # ymin = 0
-        # screen_center = 
-        pass
+        # screen_center = _get_screen_center()
+        # bias = _get_bias()
+        for f in range(len(self.g_pool.gaze_positions_by_frame)):
+            for i in range(len(self.g_pool.gaze_positions_by_frame[f])):
+                x_bias, y_bias, confident = _get_bias_by_frame(f, i)
+                gaze_pos = self.untouched_gaze_positions_by_frame[f][i]['norm_pos']
+                gaze_pos = gaze_pos[0]+x_bias, gaze_pos[1]+y_bias
+                self.g_pool.gaze_positions_by_frame[f][i]['norm_pos'] = gaze_pos
+                if not confident:
+                    self.g_pool.gaze_positions_by_frame[f][i]['confidence'] = 0.0
+
+        self.notify_all_delayed({'subject':'gaze_positions_changed'})
 
     def init_gui(self):
         logger.error('Not implemented yet.')
