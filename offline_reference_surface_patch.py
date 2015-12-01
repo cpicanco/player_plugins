f31660f9afb7b7b8a5a7ac0256f709445c4866f0
cpicanco <cpicanco@ufpa.br>
2015-12-01 15:23:11 -0300

    exploring correction with dumb mean

--- offline_reference_surface_patch.py
+++ offline_reference_surface_patch.py
@@ -49,6 +49,9 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
         self.gaze_correction_min_confidence = 0.98
         self.gaze_correction_k = 2
 
+        self.mean_correction = None
+        self.mean_correction_texture = None
+
         self.output_data = {}
 
     def gl_display_gaze_cloud(self):
@@ -95,6 +98,28 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
             glMatrixMode(GL_MODELVIEW)
             glPopMatrix()
 
+    def gl_display_mean_correction(self):
+        if self.gaze_correction_texture and self.detected:
+            # cv uses 3x3 gl uses 4x4 tranformation matricies
+            m = cvmat_to_glmat(self.m_to_screen)
+
+            glMatrixMode(GL_PROJECTION)
+            glPushMatrix()
+            glLoadIdentity()
+            glOrtho(0, 1, 0, 1,-1,1) # gl coord convention
+
+            glMatrixMode(GL_MODELVIEW)
+            glPushMatrix()
+            #apply m  to our quad - this will stretch the quad such that the ref suface will span the window extends
+            glLoadMatrixf(m)
+
+            self.mean_correction_texture.draw()
+
+            glMatrixMode(GL_PROJECTION)
+            glPopMatrix()
+            glMatrixMode(GL_MODELVIEW)
+            glPopMatrix()
+
     #### fns to draw surface in seperate window
     def gl_display_in_window(self,world_tex):
         """
@@ -134,6 +159,9 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
 
             if self.gaze_correction_texture:
                 self.gaze_correction_texture.draw()
+
+            if self.mean_correction_texture:
+                self.mean_correction_texture.draw()
 
             # now lets get recent pupil positions on this surface:
             for gp in self.gaze_on_srf:
@@ -281,7 +309,7 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
                 if p.alive:
                     kmeans_plugin_alive = True 
                     kmeans_plugin = p
-                    kmeans_plugin.load_untouched_gaze_no_notify()
+                    kmeans_plugin.alive = False
                     break
 
         def bias(gaze_block, k=2):
@@ -292,11 +320,11 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
                                        attempts=10,
                                        flags=cv2.KMEANS_RANDOM_CENTERS)
 
-            return centers.mean(axis = 0) - np.array([x_size/2.0, y_size/2.0])
+            return np.array([x_size/2.0, y_size/2.0]) - centers.mean(axis = 0) 
 
         def correction(gaze_block, bias):
-            gaze_block[:, 0] -= bias[0]
-            gaze_block[:, 1] -= bias[1]
+            gaze_block[:, 0] += bias[0]
+            gaze_block[:, 1] += bias[1]
             return gaze_block
 
         if self.cache is None:
@@ -364,7 +392,7 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
         img += 255
 
         for g in unbiased_gaze:
-            cv2.circle(img, (int(g[0]),int(g[1])), 5, (0, 0, 0), 0)
+            cv2.circle(img, (int(g[0]),int(g[1])), 3, (0, 0, 0), 0)
 
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
         _,_,centers = cv2.kmeans(unbiased_gaze.astype('float32'),2,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
@@ -372,7 +400,7 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
         for c in centers:
              cv2.circle(img, (int(c[0]),int(c[1])), 5, (0, 0, 255), -1)
 
-        bias_to_draw = [b['bias'] + [x_size, y_size] for b in bias_along_blocks]
+        bias_to_draw = [[(x_size/2.)-b['bias'][0], (y_size/2.)-b['bias'][1]] for b in bias_along_blocks]
         for b in bias_to_draw:
             cv2.circle(img, (int(b[0]),int(b[1])), 5, (255, 0, 0), -1)
 
@@ -392,6 +420,118 @@ class Offline_Reference_Surface_Extended(Offline_Reference_Surface):
         self.gaze_correction_texture.update_from_ndarray(self.gaze_correction)
 
         if kmeans_plugin_alive:
-            kmeans_plugin.gaze_correction()
+            kmeans_plugin.alive = True
+
+    def generate_mean_correction(self,section):
+        kmeans_plugin_alive = False
+        for p in self.g_pool.plugins:
+            if p.class_name == 'KMeans_Gaze_Correction':
+                if p.alive:
+                    kmeans_plugin_alive = True 
+                    kmeans_plugin = p
+                    kmeans_plugin.alive = False
+                    break
+        
+        if self.cache is None:
+            logger.error('Surface cache is not build yet.')
+            return
+
+        def bias(gaze_block):
+            x_bias = np.mean(gaze_block[:, 0])
+            y_bias = np.mean(gaze_block[:, 1])
+            return np.array([0.5-x_bias, 0.5-y_bias])
+
+        def correction(gaze_block, bias):
+            gaze_block[:, 0] += bias[0]
+            gaze_block[:, 1] += bias[1]
+            return gaze_block
+
+        all_gaze = []
+        gaze_outside_srf = 0
+        gaze_no_confidence = 0
+        no_surface = 0
+        for frame_idx,c_e in enumerate(self.cache[section]):
+            if c_e:
+                frame_idx+=section.start
+                for i, gp in enumerate(self.gaze_on_srf_by_frame_idx(frame_idx,c_e['m_from_screen'])):
+                    if gp['on_srf']:
+                        if gp['base']['confidence'] >= self.gaze_correction_min_confidence:
+                            all_gaze.append({'frame':frame_idx,'i':i,'norm_pos':gp['norm_pos']})
+                        else:
+                            gaze_no_confidence += 1
+                    else:
+                        gaze_outside_srf += 1
+            else:
+                no_surface += 1
+
+        if not all_gaze:
+            logger.error("No gaze point on surface found.")
+            return
+        else:
+            gaze_count = len(all_gaze)
+            logger.info('Found %s frames with no surface.'%no_surface)
+            logger.info("Found %s gaze points."%gaze_count)
+            logger.info("Removed %s outside surface."%gaze_outside_srf)
+            logger.info("Removed '{0}' with confidence < '{1}'".format(gaze_no_confidence, self.gaze_correction_min_confidence))
+
+        clamped_gaze = np.array([g['norm_pos'] for g in all_gaze])
+        #clamped_gaze = np.array([denormalize(g['norm_pos'], (x_size, y_size), True) for g in all_gaze]).astype('float32')
+
+        min_block_size = int(self.gaze_correction_block_size)
+
+        if gaze_count < min_block_size:
+            logger.error("Too few data to proceed.")
+            return
+
+        bias_along_blocks = []
+        unbiased_gaze = []
+        for block_start in range(0, gaze_count, min_block_size):
+            block_end = block_start + min_block_size
+            if block_end <= gaze_count:
+                gaze_block = clamped_gaze[block_start:block_end, :]
+                gaze_bias = bias(gaze_block)
+            else:
+                block_end = gaze_count
+                gaze_block = clamped_gaze[block_start:block_end, :]
+
+            bias_along_blocks.append({'bias':gaze_bias, 'block':[block_start,block_end]})
+            unbiased_gaze.append(correction(gaze_block, gaze_bias))
+
+        unbiased_gaze = np.vstack(unbiased_gaze)
+    
+        # draw
+        x_size, y_size = self.real_world_size['x'], self.real_world_size['y'] 
+        img = np.zeros((y_size,x_size,4), np.uint8)
+        img += 255
+
+        gaze_to_draw = [denormalize(g, (x_size, y_size), True) for g in unbiased_gaze]
+        for g in gaze_to_draw:
+            cv2.circle(img, (int(g[0]),int(g[1])), 3, (0, 0, 0), 0)
+
+        
+        bias_to_draw = [denormalize(b['bias'], (x_size, y_size), False) for b in bias_along_blocks]  
+        
+        # top left = (0,0)
+        bias_to_draw = [[(x_size/2.)-b[0], (y_size/2.)+b[1]] for b in bias_to_draw]
+
+        for b in bias_to_draw:
+            cv2.circle(img, (int(b[0]),int(b[1])), 5, (255, 0, 0), -1)
+
+        unbiased_gaze = [{'frame':g['frame'], 'i': g['i'], 'gaze':unbiased_gaze[i]} for i, g in enumerate(all_gaze)]
+
+        self.output_data['unbiased_gaze_mean'] = unbiased_gaze
+        self.output_data['bias_along_blocks_mean'] = bias_along_blocks
+
+        alpha = img.copy()
+        alpha -= .5*255
+        alpha *= -1
+        img[:,:,3] = alpha[:,:,0]
+
+        self.mean_correction = img
+        self.mean_correction_texture = Named_Texture()
+        self.mean_correction_texture.update_from_ndarray(self.mean_correction)
+
+        if kmeans_plugin_alive:
+            kmeans_plugin.alive = True
 
 del Offline_Reference_Surface
