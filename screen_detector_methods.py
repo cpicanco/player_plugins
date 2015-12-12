51710ab00921802e47b6c455089bdcf34c9e5f8e
cpicanco <cpicanco@ufpa.br>
2015-12-11 22:30:44 -0300

    use adaptative thresh in s detector

--- screen_detector_methods.py
+++ screen_detector_methods.py
@@ -19,13 +19,24 @@ from quad_segmentation import sortCorners
 
 # hacked detect_markers
 # now it detects a single outmost contour/screen instead
-# "screens" is just for compatibility with the marker detector
-def detect_screens(gray_img,grid_size):
-    _ , edges = cv2.threshold(gray_img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+# "screens" container is just for compatibility with the marker detector
+def detect_screens(gray_img, draw_contours=False):
+    edges = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 17, -2)
 
-    contours, _ = cv2.findContours(edges,
+    contours, hierarchy = cv2.findContours(edges,
                                     mode=cv2.RETR_TREE,
                                     method=cv2.CHAIN_APPROX_SIMPLE,offset=(0,0)) #TC89_KCOS
+    
+    if draw_contours:
+        cv2.drawContours(gray_img, contours,-1, (0,0,0))
+    
+    # remove extra encapsulation
+    hierarchy = hierarchy[0]
+    contours = np.array(contours)
+
+    # keep only contours                        with parents     and      children
+    contours = contours[np.logical_and(hierarchy[:,3]>=0, hierarchy[:,2]>=0)]
+
     contours = np.array(contours)
     screens = []
     if contours != None: 
@@ -37,44 +48,52 @@ def detect_screens(gray_img,grid_size):
             epsilon = cv2.arcLength(contours[0], True)*0.1
 
             # find the volatile vertices of the contour
-            aprox_contours = [cv2.approxPolyDP(contours[0], epsilon, True)];
-            
+            aprox_contours = [cv2.approxPolyDP(contours[0], epsilon, True)]
+
+            # we want all contours to be counter clockwise oriented, we use convex hull for this:
+            # aprox_contours = [cv2.convexHull(c,clockwise=True) for c in aprox_contours if c.shape[0]==4]
+
+            # a non convex quadrangle is not what we are looking for.
             rect_cand = [r for r in aprox_contours if r.shape[0]==4]
 
-            # screens 
-            size = 10*grid_size
-            #top left,bottom left, bottom right, top right in image
-            mapped_space = np.array( ((0,0),(size,0),(size,size),(0,size)) ,dtype=np.float32).reshape(4,1,2)
+            # if draw_contours:
+            #     cv2.drawContours(gray_img, rect_cand,-1, (0,0,0))
+
+            # screens
             for r in rect_cand:
                 r = np.float32(r)
 
-                screen = 0, 1
-                if screen is not None:
-                    angle,msg = screen
+                msg = 1
 
-                    # define the criteria to stop and refine the screen verts
-                    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-                    cv2.cornerSubPix(gray_img,r,(3,3),(-1,-1),criteria)
+                # define the criteria to stop and refine the screen verts
+                criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+                cv2.cornerSubPix(gray_img,r,(3,3),(-1,-1),criteria)
 
-                    centroid = r.sum(axis=0)/4.
-                    centroid.shape = (2)
+                corners = np.array([r[0][0], r[1][0], r[2][0], r[3][0]])
 
-                    corners = np.array([r[0][0], r[1][0], r[2][0], r[3][0]])
+                # we need the centroid of the screen
+                # M = cv2.moments(corners.reshape(-1,1,2))
+                # centroid = np.array([M['m10']/M['m00'], M['m01']/M['m00']])
+                # print 'a', centroid
 
-                    center = [0, 0]
-                    for i in corners:
-                        center += i
-                    center *= (1. / len(corners))
+                centroid = corners.sum(axis=0, dtype='float64')*0.25
+                centroid.shape = (2)
+                # print 'b', centroid
 
-                    corners = sortCorners(corners, center)
-                    r[0][0], r[1][0], r[2][0], r[3][0] = corners[0], corners[1], corners[2], corners[3]
+                # do not force dtype, use system default instead
+                # centroid = [0, 0]
+                # for i in corners:
+                #     centroid += i
+                # centroid *= (1. / len(corners))
+                # print 'c', centroid
 
-                    r_norm = r/np.float32((gray_img.shape[1],gray_img.shape[0]))
-                    r_norm[:,:,1] = 1-r_norm[:,:,1]
-                    screen = {'id':msg,'verts':r,'verts_norm':r_norm,'centroid':centroid,"frames_since_true_detection":0}
+                corners = sortCorners(corners, centroid)
+                r[0][0], r[1][0], r[2][0], r[3][0] = corners[0], corners[1], corners[2], corners[3]
 
-                    screens.append(screen)
-
+                r_norm = r/np.float32((gray_img.shape[1],gray_img.shape[0]))
+                r_norm[:,:,1] = 1-r_norm[:,:,1]
+                screen = {'id':msg,'verts':r,'verts_norm':r_norm,'centroid':centroid,"frames_since_true_detection":0}
+                screens.append(screen)
     return screens
 
 #persistent vars for detect_markers_robust
@@ -91,7 +110,7 @@ def detect_markers_robust(gray_img,grid_size,prev_markers,min_marker_perimeter=4
     global tick
     if not tick:
         tick = true_detect_every_frame
-        new_markers = detect_screens(gray_img,grid_size) # ,min_marker_perimeter,aperture,visualize)
+        new_markers = detect_screens(gray_img) # ,min_marker_perimeter,aperture,visualize)
     else:
         new_markers = []
     tick -=1
@@ -105,18 +124,23 @@ def detect_markers_robust(gray_img,grid_size,prev_markers,min_marker_perimeter=4
         not_found = [m for m in prev_markers if m['id'] not in new_ids and m['id'] >=0]
         if not_found:
             prev_pts = np.array([m['centroid'] for m in not_found])
-            # new_pts, flow_found, err = cv2.calcOpticalFlowPyrLK(prev_img, gray_img,prev_pts,winSize=(100,100))
-            new_pts, flow_found, err = cv2.calcOpticalFlowPyrLK(prev_img, gray_img,prev_pts,minEigThreshold=0.01,**lk_params)
-            for pt,s,e,m in zip(new_pts,flow_found,err,not_found):
-                if s: #ho do we ensure that this is a good move?
-                    m['verts'] += pt-m['centroid'] #uniformly translate verts by optlical flow offset
-                    r_norm = m['verts']/np.float32((gray_img.shape[1],gray_img.shape[0]))
-                    r_norm[:,:,1] = 1-r_norm[:,:,1]
-                    m['verts_norm'] = r_norm
-                    m["frames_since_true_detection"] +=1
-                else:
-                    m["frames_since_true_detection"] =100
-
+            try:
+                # new_pts, flow_found, err = cv2.calcOpticalFlowPyrLK(prev_img, gray_img,prev_pts,winSize=(100,100))
+                new_pts, flow_found, err = cv2.calcOpticalFlowPyrLK(prev_img, gray_img,prev_pts,minEigThreshold=0.01,**lk_params)
+                for pt,s,e,m in zip(new_pts,flow_found,err,not_found):
+                    if s: #ho do we ensure that this is a good move?
+                        m['verts'] += pt-m['centroid'] #uniformly translate verts by optlical flow offset
+                        r_norm = m['verts']/np.float32((gray_img.shape[1],gray_img.shape[0]))
+                        r_norm[:,:,1] = 1-r_norm[:,:,1]
+                        m['verts_norm'] = r_norm
+                        m["frames_since_true_detection"] +=1
+                    else:
+                        m["frames_since_true_detection"] =100
+                
+            except Exception, e:
+                # OpenCV Assertion Error calcOpticalFlowPyrLK
+                # print e
+                pass
 
         #cocatenating like this will favour older markers in the doublication deletion process
         markers = [m for m in not_found if m["frames_since_true_detection"] < 10 ]+new_markers
