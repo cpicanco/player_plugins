0e1932e4ca06292ccd5a3d9cb8fed54ecb3b89e7
cpicanco <cpicanco@ufpa.br>
2016-02-07 12:46:12 -0300

    refactoring, homography test not functional yet

--- self_contained/homography_test.py
+++ self_contained/homography_test.py
@@ -1,95 +1,139 @@
+import cv2
+import numpy as np
 
+def sortCorners(corners, center):
+    """
+    corners : list of points 
+    center : point
+    """
+    top = []
+    bot = []
+
+    for i in corners:
+        if i[1] < center[1]:
+            top.append(i)
+        else:
+            bot.append(i)
+
+    corners = np.zeros(shape=(4,2))
+
+    if (len(top) == 2) and (len(bot) == 2):
+        # top left
+        if top[0][0] > top[1][0]:
+            tl = top[1]
+        else:
+            tl = top[0]
+
+        # top right
+        if top[0][0] > top[1][0]:
+            tr = top[0]
+        else:
+            tr = top[1]
+
+        # botton left
+        if bot[0][0] > bot[1][0]:
+            bl = bot[1]
+        else:
+            bl = bot[0]
+
+        # botton right
+        if bot[0][0] > bot[1][0]:
+            br = bot[0]
+        else:
+            br = bot[1]
+
+    corners[0] = np.array(tl)
+    corners[1] = np.array(tr)
+    corners[2] = np.array(br)
+    corners[3] = np.array(bl)
+
+    return corners
+
+def m_verts_to_screen(verts, mapping):
+    return cv2.getPerspectiveTransform(mapping,verts)
+
+def m_verts_from_screen(verts, mapping):
+    return cv2.getPerspectiveTransform(verts,mapping)
+
+def detect_screens(gray_img, draw_contours=False):
+    edges = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 17, -2)
+
+    contours, hierarchy = cv2.findContours(edges,
+                                    mode=cv2.RETR_TREE,
+                                    method=cv2.CHAIN_APPROX_SIMPLE,offset=(0,0)) #TC89_KCOS
+    
+    if draw_contours:
+        cv2.drawContours(gray_img, contours,-1, (0,0,0))
+    
+    # remove extra encapsulation
+    hierarchy = hierarchy[0]
+    contours = np.array(contours)
+
+    # keep only contours                        with parents     and      children
+    contours = contours[np.logical_and(hierarchy[:,3]>=0, hierarchy[:,2]>=0)]
+
+    contours = np.array(contours)
+    screens = []
+    if contours != None: 
+        # keep only > thresh_area   
+        contours = [c for c in contours if cv2.contourArea(c) > (20 * 2500)]
+        
+        if len(contours) > 0: 
+            # epsilon is a precision parameter, here we use 10% of the arc
+            epsilon = cv2.arcLength(contours[0], True)*0.1
+
+            # find the volatile vertices of the contour
+            aprox_contours = [cv2.approxPolyDP(contours[0], epsilon, True)]
+
+            # we want all contours to be counter clockwise oriented, we use convex hull for this:
+            aprox_contours = [cv2.convexHull(c,clockwise=True) for c in aprox_contours if c.shape[0]==4]
+
+            # a non convex quadrangle is not what we are looking for.
+            rect_cand = [r for r in aprox_contours if r.shape[0]==4]
+
+            # if draw_contours:
+            #     cv2.drawContours(gray_img, rect_cand,-1, (0,0,0))
+
+            # screens
+            for r in rect_cand:
+                r = np.float32(r)
+
+                msg = 1
+
+                # define the criteria to stop and refine the screen verts
+                criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
+                cv2.cornerSubPix(gray_img,r,(3,3),(-1,-1),criteria)
+
+                corners = np.array([r[0][0], r[1][0], r[2][0], r[3][0]])
+
+                # we need the centroid of the screen
+                M = cv2.moments(corners.reshape(-1,1,2))
+                centroid = np.array([M['m10']/M['m00'], M['m01']/M['m00']])
+                print 'a', centroid
+
+                # centroid = corners.sum(axis=0, dtype='float64')*0.25
+                # centroid.shape = (2)
+                # print 'b', centroid
+
+                # do not force dtype, use system default instead
+                # centroid = [0, 0]
+                # for i in corners:
+                #     centroid += i
+                # centroid *= (1. / len(corners))
+                # print 'c', centroid
+
+                # corners = sortCorners(corners, centroid)
+
+
+                # r[0][0], r[1][0], r[2][0], r[3][0] = corners[0], corners[1], corners[2], corners[3]
+
+                r_norm = r/np.float32((gray_img.shape[1],gray_img.shape[0]))
+                r_norm[:,:,1] = 1-r_norm[:,:,1]
+                screen = {'id':msg,'verts':r,'verts_norm':r_norm,'centroid':centroid,"frames_since_true_detection":0}
+                screens.append(screen)
+    return screens
 
 if __name__ == '__main__':
-    import cv2
-    import numpy as np
-
-    from quad_segmentation import sortCorners
-
-    def m_verts_to_screen(verts, mapping):
-        return cv2.getPerspectiveTransform(mapping,verts)
-
-    def m_verts_from_screen(verts, mapping):
-        return cv2.getPerspectiveTransform(verts,mapping)
-
-    def detect_screens(gray_img, draw_contours=False):
-        edges = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 17, -2)
-
-        contours, hierarchy = cv2.findContours(edges,
-                                        mode=cv2.RETR_TREE,
-                                        method=cv2.CHAIN_APPROX_SIMPLE,offset=(0,0)) #TC89_KCOS
-        
-        if draw_contours:
-            cv2.drawContours(gray_img, contours,-1, (0,0,0))
-        
-        # remove extra encapsulation
-        hierarchy = hierarchy[0]
-        contours = np.array(contours)
-
-        # keep only contours                        with parents     and      children
-        contours = contours[np.logical_and(hierarchy[:,3]>=0, hierarchy[:,2]>=0)]
-
-        contours = np.array(contours)
-        screens = []
-        if contours != None: 
-            # keep only > thresh_area   
-            contours = [c for c in contours if cv2.contourArea(c) > (20 * 2500)]
-            
-            if len(contours) > 0: 
-                # epsilon is a precision parameter, here we use 10% of the arc
-                epsilon = cv2.arcLength(contours[0], True)*0.1
-
-                # find the volatile vertices of the contour
-                aprox_contours = [cv2.approxPolyDP(contours[0], epsilon, True)]
-
-                # we want all contours to be counter clockwise oriented, we use convex hull for this:
-                aprox_contours = [cv2.convexHull(c,clockwise=True) for c in aprox_contours if c.shape[0]==4]
-
-                # a non convex quadrangle is not what we are looking for.
-                rect_cand = [r for r in aprox_contours if r.shape[0]==4]
- 
-                # if draw_contours:
-                #     cv2.drawContours(gray_img, rect_cand,-1, (0,0,0))
-
-                # screens
-                for r in rect_cand:
-                    r = np.float32(r)
-
-                    msg = 1
-
-                    # define the criteria to stop and refine the screen verts
-                    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.001)
-                    cv2.cornerSubPix(gray_img,r,(3,3),(-1,-1),criteria)
-
-                    corners = np.array([r[0][0], r[1][0], r[2][0], r[3][0]])
-
-                    # we need the centroid of the screen
-                    M = cv2.moments(corners.reshape(-1,1,2))
-                    centroid = np.array([M['m10']/M['m00'], M['m01']/M['m00']])
-                    print 'a', centroid
-
-                    # centroid = corners.sum(axis=0, dtype='float64')*0.25
-                    # centroid.shape = (2)
-                    # print 'b', centroid
-
-                    # do not force dtype, use system default instead
-                    # centroid = [0, 0]
-                    # for i in corners:
-                    #     centroid += i
-                    # centroid *= (1. / len(corners))
-                    # print 'c', centroid
-
-                    # corners = sortCorners(corners, centroid)
-
-
-                    # r[0][0], r[1][0], r[2][0], r[3][0] = corners[0], corners[1], corners[2], corners[3]
-
-                    r_norm = r/np.float32((gray_img.shape[1],gray_img.shape[0]))
-                    r_norm[:,:,1] = 1-r_norm[:,:,1]
-                    screen = {'id':msg,'verts':r,'verts_norm':r_norm,'centroid':centroid,"frames_since_true_detection":0}
-                    screens.append(screen)
-        return screens
-
     real_shape = np.array([ 1280., 768. ])
     bias = np.array([ 41.60437012,  96.76702881])
 
@@ -100,7 +144,6 @@ if __name__ == '__main__':
     mapped_space_uv = np.array(((0,0),(1,0),(1,1),(0,1)),dtype=np.float32)
     mapped_space_to_screen = mapped_space_uv.reshape(-1,1,2)
     mapped_space_image = np.array(((0,0),(real_shape[0],0),(real_shape[0],real_shape[1]),(0,real_shape[1])),dtype=np.float32)
-
 
     #point = cv2.perspectiveTransform(bias, m_to_screen)
 
@@ -138,7 +181,7 @@ if __name__ == '__main__':
     for i,v in enumerate(s_vertices):
         cv2.circle(gray_world_img, (int(v[0]),int(v[1])), 3, (0, 0, 0), -1)
         cv2.putText(gray_world_img, str(i), (int(v[0] -10),int(v[1]) -10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,0), 1, lineType = cv2.CV_AA )
-    
+
 
     cv2.circle(gray_world_img, (int(s_centroid[0]),int(s_centroid[1])), 3, (0, 0, 0), 0)
 
